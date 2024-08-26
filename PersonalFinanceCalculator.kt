0800fc24e7330be6104a42bfb9ec415b708b6a7b
MDeMel-Dev <manendra.demel@beemit.com.au>
2024-08-26 21:30:26 +1000

    save kotlin exercise files

---
+++ PersonalFinanceCalculator.kt
@@ -0,0 +1,20 @@
+package com.vu.myapplication
+
+class PersonalFinanceCalculator {
+
+    val transactionList = mutableListOf<Transaction>()
+
+    fun printGreeting() {
+        println("Greetings! I am your personal finance calculator.")
+    }
+
+    fun saveTransaction(transaction: Transaction) {
+        transactionList.add(transaction)
+    }
+
+    fun printAllTransactions() {
+        transactionList.forEach {
+            println("${it.name} $${it.getValue()}")
+        }
+    }
+}
